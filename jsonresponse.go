3783a07b22e15cd8019472d4ee4e3612f8bef27e
Yinyin L <yinyinl@gmail.com>
2024-07-11 03:26:15 +0800

    add function to generate JSON response

---
+++ jsonresponse.go
@@ -0,0 +1,30 @@
+package grpchttpendpoint
+
+import (
+	"net/http"
+	"strconv"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
+
+func attachJSONContentHeader(w http.ResponseWriter, b []byte) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Content-Length", strconv.FormatInt(int64(len(b)), 10))
+}
+
+// JSONResponseWithStatusCode generate JSON response based on given protobuf
+// message m with given statusCode and private Cache-Control headers enabled.
+func JSONResponseWithStatusCode(w http.ResponseWriter, m proto.Message, statusCode int) (err error) {
+	b, err := protojson.Marshal(m)
+	if err != nil {
+		http.Error(w, "500 JSONResponseWithStatusCode Failed:\n"+err.Error(), http.StatusInternalServerError)
+		return err
+	}
+	attachJSONContentHeader(w, b)
+	w.Header().Set("Cache-Control", "no-cache, private")
+	w.Header().Set("Pragma", "no-cache")
+	w.WriteHeader(statusCode)
+	w.Write(b)
+	return nil
+}
